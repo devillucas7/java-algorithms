8e2723308d5aa28812865e2a24c1debf40abc7b3
devillucas7 <113412584+devillucas7@users.noreply.github.com>
2022-10-04 20:23:20 +0530

    bubblesort

--- bubblesort.java
+++ bubblesort.java
@@ -1,50 +1,28 @@
-import java.io.*;
- 
-class GFG
-{
-    // An optimized version of Bubble Sort
-    static void bubbleSort(int arr[], int n)
-    {
-        int i, j, temp;
-        boolean swapped;
-        for (i = 0; i < n - 1; i++)
-        {
-            swapped = false;
-            for (j = 0; j < n - i - 1; j++)
-            {
-                if (arr[j] > arr[j + 1])
-                {
-                    // swap arr[j] and arr[j+1]
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
-                }
-            }
- 
-            // IF no two elements were
-            // swapped by inner loop, then break
-            if (swapped == false)
-                break;
-        }
-    }
- 
-    // Function to print an array
-    static void printArray(int arr[], int size)
-    {
-        int i;
-        for (i = 0; i < size; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
- 
-    // Driver program
-    public static void main(String args[])
-    {
-        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-        int n = arr.length;
-        bubbleSort(arr, n);
-        System.out.println("Sorted array: ");
-        printArray(arr, n);
-    }
+public static void main(String[] args)
+ {
+
+  int number[]={7,13,4,5,62,3,1,3,45};
+
+  int temp;
+  boolean fixed=false;
+  while(fixed==false){
+      fixed = true;
+
+  for (int i=0; i <number.length-1;i++){ 
+      if (number[i]>number[i+1]){
+
+      temp = number [i+1];
+
+      number[i+1]=number[i];
+
+      number[i]=temp;
+      fixed=false;
+      }
+ }
+}
+  for (int i=0; i<number.length;i++){
+      System.out.println(number[i]);
+   }
+  }
+
 }
